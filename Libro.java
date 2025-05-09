93013f1e2821e1182e445bb9c92e56bd7cf1cf8d
luised89 <lupenar@unal.edu.co>
2025-05-08 19:06:07 -0500

    Update Libro.java

--- Libro.java
+++ Libro.java
@@ -8,17 +8,21 @@ package registro;
  *
  * @author Estudiante
  */
+import java.util.ArrayList;
+
 public class Libro {
     
     public String Titulo;
     public int añoDePublicacion;
     public String Genero;
     
+    public static ArrayList<Libro> listaLibros = new ArrayList<>();
+    
     
     public Libro(String tit, int aDp, String Gnr) {
         this.Titulo = tit;
         this.añoDePublicacion = aDp;
         this.Genero = Gnr;
+        listaLibros.add(this);
     }
 }
-
